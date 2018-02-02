ee79c3e65b36720bd9e299539ebf6089374e546e
Steven Molen <darthmolen@gmail.com>
2018-02-02 14:15:54 -0600

    First Commit with a hello world bot and some business objects. In Web Api.

---
+++ PF.Redmine.Bot.Domain/ProjectTracker.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PF.Redmine.Bot.Domain
+{
+    /*public class Project
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }*/
+
+    public class Tracker
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Status
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Priority
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    /*public class Author
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }*/
+
+    public class AssignedTo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class FixedVersion
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class CustomField
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public object internal_name { get; set; }
+        public string value { get; set; }
+    }
+
+    public class Issue
+    {
+        public int id { get; set; }
+        public Project project { get; set; }
+        public Tracker tracker { get; set; }
+        public Status status { get; set; }
+        public Priority priority { get; set; }
+        public Author author { get; set; }
+        public AssignedTo assigned_to { get; set; }
+        public FixedVersion fixed_version { get; set; }
+        public string subject { get; set; }
+        public string description { get; set; }
+        public string start_date { get; set; }
+        public string due_date { get; set; }
+        public int done_ratio { get; set; }
+        public double spent_hours { get; set; }
+        public List<CustomField> custom_fields { get; set; }
+        public string created_on { get; set; }
+        public string updated_on { get; set; }
+    }
+
+
+    public class Author
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public object easy_external_id { get; set; }
+    }
+
+    public class Parent
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Project
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string homepage { get; set; }
+        public int status { get; set; }
+        public string easy_start_date { get; set; }
+        public Author author { get; set; }
+        public string created_on { get; set; }
+        public string updated_on { get; set; }
+        public Parent parent { get; set; }
+        public string easy_due_date { get; set; }
+    }
+
+    public class Projects
+    {
+        public List<Project> projects { get; set; }
+        public int total_count { get; set; }
+        public int offset { get; set; }
+        public int limit { get; set; }
+    }
+
+    public class Issues
+    {
+        public List<Issue> issues { get; set; }
+        public int total_count { get; set; }
+        public int offset { get; set; }
+        public int limit { get; set; }
+    }
+
+    public class DisplayIssue
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int status { get; set; }
+    }
+
+    public class DisplayProject
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Token
+    {
+        public string action { get; set; }
+        public string value { get; set; }
+    }
+
+    public class User
+    {
+        public int id { get; set; }
+        public string login { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string mail { get; set; }
+        public string created_on { get; set; }
+        public string api_key { get; set; }
+        public List<Token> tokens { get; set; }
+        public string last_login_on { get; set; }
+    }
+
+    public class Users
+    {
+        public List<User> users { get; set; }
+        public int total_count { get; set; }
+        public int offset { get; set; }
+        public int limit { get; set; }
+    }
+
+    public class DisplayUser
+    {
+        public int id { get; set; }
+        public string login { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ PF.Redmine.Bot/DataProviders/ProjectTrackerClient.cs
@@ -0,0 +1,194 @@
+﻿using PF.Redmine.Bot.Domain;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace PF.Redmine.Bot.DataProviders
+{
+    class ProjectTrackerClient
+    {
+        public string ApiKey { get; private set; }
+        public ProjectTrackerClient(string apiKey)
+        {
+            ApiKey = apiKey;
+        }
+        public Issue GetIssue(int IssueNum)
+        {
+            var issueUri = String.Format("/issues/{0}.json", IssueNum);
+
+            var respStr = GetResponse(issueUri);
+
+            using (var rdr = new StringReader(respStr))
+            using (var jReader = new JsonTextReader(rdr))
+            {
+                var jObj = JObject.ReadFrom(jReader);
+
+                var i = jObj["issue"];
+
+                var convIssue = JsonConvert.DeserializeObject<Issue>(i.ToString());
+
+                return convIssue;
+            }
+        }
+
+        public Issues GetPagedIssuesForProject(int ProjectId, int offset, int limit)
+        {
+            var issuesUri = String.Format("/issues.json?project_id={0}&status_id=open&offset={1}&limit={2}&sort=updated_on:desc", ProjectId, offset, limit);
+
+            var respStr = GetResponse(issuesUri);
+
+            var convIssues = JsonConvert.DeserializeObject<Issues>(respStr);
+
+            return convIssues;
+        }
+
+        public Issues GetPagedIssuesForUser(int UserId, int offset, int limit)
+        {
+            var issuesUri = String.Format("/issues.json?assigned_to_id={0}&offset={1}&limit={2}&sort=updated_on:desc", UserId, offset, limit);
+
+            var respStr = GetResponse(issuesUri);
+
+            var convIssues = JsonConvert.DeserializeObject<Issues>(respStr);
+
+            return convIssues;
+        }
+
+        public List<DisplayProject> GetAllProjects()
+        {
+            var firstPage = GetPagedProjects(0, 100);
+
+            List<DisplayProject> allProjects = (
+                from project in firstPage.projects
+                select new DisplayProject { id = project.id, name = project.name }).ToList<DisplayProject>();
+
+            var total = firstPage.total_count;
+
+            for (var i = firstPage.offset + 100; i <= total; i = i + 100)
+            {
+                var page = GetPagedProjects(i, 100);
+
+                if (page.projects != null && page.projects.Count > 0)
+                {
+                    allProjects.AddRange(
+                        (from project in firstPage.projects
+                         select new DisplayProject { id = project.id, name = project.name }));
+                }
+            }
+
+            return allProjects;
+        }
+
+        public Projects GetPagedProjects(int offset, int limit)
+        {
+            var projectUri = String.Format("/projects.json?offset={0}&limit={1}", offset, limit);
+
+            var respStr = GetResponse(projectUri);
+
+            var convProjects = JsonConvert.DeserializeObject<Projects>(respStr);
+
+            return convProjects;
+        }
+
+        public Users GetPagedUsers(int offset, int limit)
+        {
+            var usersUri = String.Format("/users.json?offset={0}&limit={1}", offset, limit);
+
+            var respStr = GetResponse(usersUri);
+
+            var convUsers = JsonConvert.DeserializeObject<Users>(respStr);
+
+            return convUsers;
+        }
+
+        public List<DisplayUser> GetAllUsers()
+        {
+            var firstPage = GetPagedUsers(0, 100);
+            var total_count = firstPage.total_count;
+
+            List<DisplayUser> allUsers = (from user in firstPage.users
+                                          select new DisplayUser() { id = user.id, login = user.login }).ToList<DisplayUser>();
+
+            for (var i = firstPage.offset + 100; i <= total_count; i = i + 100)
+            {
+                var page = GetPagedUsers(i, 100);
+
+                if (page.users != null && page.users.Count > 0)
+                {
+                    allUsers.AddRange(
+                        (from user in page.users
+                         select new DisplayUser() { id = user.id, login = user.login, name = String.Format("{0} {1}", user.firstname, user.lastname) }
+                        ));
+                }
+            }
+
+            return allUsers;
+        }
+
+        #region Private Methods
+
+        public string GetResponse(string requestUri)
+        {
+
+            string ret = "";
+
+            var request = System.Net.WebRequest.Create(String.Format("{0}/{1}", GetApiUrl().TrimEnd('/'), requestUri.TrimStart('/')));
+            request.ContentType = "application/json";
+            request.Headers.Add("X-Redmine-API-Key", GetApiKey());
+
+            var resp = request.GetResponse();
+
+            try
+            {
+
+                if (((HttpWebResponse)resp).StatusCode == HttpStatusCode.OK)
+                {
+                    using (Stream dataStream = resp.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(dataStream))
+                    {
+                        ret = reader.ReadToEnd();
+                        reader.Close();
+                        dataStream.Close();
+                    }
+                }
+                else
+                {
+                    var respex = (HttpWebResponse)resp;
+
+                    throw new Exception(String.Format("Response Exception: {0} | {1}", respex.StatusCode, respex.StatusDescription));
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                resp.Close();
+            }
+
+
+            return ret;
+
+        }
+
+        private static string GetApiUrl()
+        {
+            return ConfigurationManager.AppSettings["PtUrl"];
+        }
+
+        private string GetApiKey()
+        {
+            //return ConfigurationManager.AppSettings["PtKey"];
+            return this.ApiKey;
+        }
+
+        #endregion Private Methods
+
+    }
+}
